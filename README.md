f7e4703463c02c46e76d2d947d876f8e85d6a0b1
Kyle Brown <brownkylej@gmail.com>
2019-10-01 10:40:00 -0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Newspaper
+
+[Use it here](https://newspaper.genericlastname.now.sh/)
+
+The first interactive JS website I ever made. Just a simple toy that modifies the DOM to change the theme. It was also an experiment in responsive CSS.
--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let theme = 'news';
 
 function changeTheme() {
+  header = document.getElementById('theme');
+  header.textContent = 'The theme is currently ' + theme +
+    '. Click anywhere to change.';
+
   if (theme == 'sepia') {
     document.body.style.backgroundColor = '#ffe3c7';
     document.body.style.color = '#875f37';
     theme = 'news';
   } else if (theme == 'news') {
-    document.body.style.backgroundColor = '#e3e3e3';
+    document.body.style.backgroundColor = '#f0f0f0';
     document.body.style.color = '#424242';
     theme = 'twilight'
   } else if (theme == 'twilight') {
@@ -15,7 +19,7 @@ function changeTheme() {
     theme = 'night'
   } else if (theme == 'night') {
     document.body.style.backgroundColor = '#1f1f1f';
-    document.body.style.color = '#7a7a7a';
+    document.body.style.color = '#a6a6a6';
     theme = 'sepia'
   }
 }
